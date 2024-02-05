732274aea7b4eb7832c13109abd458258901b517
Doyup Kim <d25kim@uwaterloo.ca>
2024-02-05 18:10:58 -0330

    added fonts, images, and basic styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,30 +1,59 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import Logo from "./logo.png";
-import LinkedInLogo from "./linkedInLogo.png"
-import GitHubLogo from "./gitHub.png"
+import Logo from "../../assets/img/logo.png";
+import LinkedInLogo from "../../assets/img/linkedInLogo.png";
+import GitHubLogo from "../../assets/img/gitHub.png";
+import icon from "../../assets/img/logo.png"
 
 const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={Logo} alt="Logo" />
+          <img src={icon} alt="Logo" style={{filter: 'invert(100%)'}}/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/doyupkim/"><img src={LinkedInLogo} alt="LinkedIn" /></a>
-              <a href="https://github.com/danwlk?tab=repositories"><img src={GitHubLogo} alt="Github" /></a>
+              <a href="https://www.linkedin.com/in/doyupkim/">
+                <img src={LinkedInLogo} alt="LinkedIn" />
+              </a>
+              <a href="https://github.com/danwlk?tab=repositories">
+                <img src={GitHubLogo} alt="Github" />
+              </a>
             </div>
+            <button className="vvd" onClick={() => console.log("Connect")}>
+              <span>Let's Connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
