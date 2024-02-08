c3fe9c7a84af5ac4c55816d07948819e9ee7f872
Doyup Kim <d25kim@uwaterloo.ca>
2024-02-08 11:49:31 -0330

    changed tab logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { useEffect } from "react";
-import "./App.css"
+import "./App.css";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
+  const useFavicon = (href) => {
+    useEffect(() => {
+      const link = document.createElement('link');
+      const existingLink = document.querySelector('link[rel="shortcut icon"]') || document.querySelector('link[rel="icon"]');
+      
+      if (existingLink) {
+        existingLink.href = href;
+      } else {
+        link.rel = 'icon';
+        link.href = href;
+        document.head.appendChild(link);
+      }
+  
+      return () => {
+        if (existingLink) {
+          document.head.removeChild(link);
+        }
+      };
+    }, [href]);
+  };
+
   useEffect(() => {
-    document.title = "Welcome to Doyup's Portfolio!"
-  }, [])
+    document.title = "Welcome to Doyup's Portfolio!";
+  }, []);
+
+  useFavicon('./img/logo.png');
 
   return (
     <div className="App">
