85e6ea2e6f50d48a8256563fddb403c980eb6cf9
Doyup Kim <d25kim@uwaterloo.ca>
2024-02-20 03:01:03 +0900

    added navbar links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -73,7 +73,7 @@ const Projects = () => {
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,7 +3,7 @@ import SkillsSphere from "./SkillsSphere";
 
 const Skills = () => {
   return (
-    <div className="skill">
+    <div className="skill" id="skills">
       <div className="skill-box">
         <h1 className="skill-title">SKILLS</h1>
         <h4 className="skill-description">Here are some skills that I am familiar with. Click on the text to see my proficiency.</h4>
